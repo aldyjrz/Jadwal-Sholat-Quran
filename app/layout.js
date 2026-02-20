ee0650c54264a1466683b1deae370fb299626fd0
Aldi Pranata <aldyjrz@gmail.com>
2026-02-21 00:14:42 +0700

    add random hadist from bukhari and muslim

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
   themeColor: '#ffffff',
   icons: {
-    icon: '/favicon.ico',
+    icon: '/vercel.svg',
   },
 };
 
